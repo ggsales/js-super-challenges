0a884e3b60a9e57ec1e8e65f34a3033687500282
ggsales <giovanisales14@gmail.com>
2021-08-03 22:51:38 -0300

    fix last details in logic and structure

--- challenge-ano-nascimento/script.js
+++ challenge-ano-nascimento/script.js
@@ -1,36 +1,44 @@
 
-function pegaValor(){ 
-    let idadeDigitada = document.getElementById("valorIdade").value; //pega valor digitado pelo usuario
+let idadeDigitada = null
+let subYear = 0
+
+function handleIdade(){ 
+    idadeDigitada = document.getElementById("valorIdade").value; //pega valor digitado pelo usuario
     if(idadeDigitada.length < 4){
-        document.getElementById("idade").innerHTML=""
-        document.getElementById("saida").innerText=""
-        console.log("saiu")
-         return  
-        }
-        
-    let subYear = 0 // Variavel que será responsavel pelo resultado da subtração de ambos os anos
+        clearData()
+        return
+    }        
+    
+    calcIdade()
+
+    showContentByIdadeUser()
+}
+
+function clearData(){
+    document.getElementById("idade").innerHTML=""
+    document.getElementById("saida").innerText=""
+}
+
+
+function calcIdade(){
+    // Variavel que será responsavel pelo resultado da subtração de ambos os anos
     let anoAtual =  new Date().getFullYear(); // Variavel que contem o ano atual
     subYear = Math.abs(anoAtual - idadeDigitada); // subtração do ano que o usuario digita - variavel do ano atual informado acima
     document.getElementById("idade").innerHTML = subYear + '  '+'anos';
-    
+}
 
+
+function showContentByIdadeUser(){
     if(subYear >= 18){
         document.getElementById("saida").innerText = "Você pode acessar o conteudo infantil, adolescente e também o adulto. ";
-        return
-    } else {
-        document.getElementById("saida").innerText  ="Você pode acessar apenas o conteudo infantil e adolescente."
-    }  
-    if(subYear <=12){
-        document.getElementById("saida").innerText  ="Você pode acessar apenas o conteudo infantil."
+    } 
+    else if(subYear < 18 && subYear >= 12){
+        document.getElementById("saida").innerText  ="Você pode acessar apenas o conteudo infantil e adolescente."    
     }
- }
-
-
-
-
-
-
-
+    else {
+        document.getElementById("saida").innerText  ="Você pode acessar apenas o conteudo infantil."       
+    } 
+}
 
   
 
